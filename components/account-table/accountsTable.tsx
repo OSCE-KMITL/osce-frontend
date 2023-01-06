3fc076610ac562a0d3c046d1930e6c88477e3da6
aphisit-ths <aphisit.thu@gmail.com>
2023-01-07 04:05:20 +0700

    install package and init apollo client .

--- components/account-table/accountsTable.tsx
+++ components/account-table/accountsTable.tsx
@@ -17,6 +17,9 @@ const AccountsTable: React.FC<AccountTableProps> = ({
 }) => {
   return (
     <Table
+      rowClassName={(record, index) =>
+        index % 2 === 0 ? "bg-gray-50" : "bg-gray-200"
+      }
       dataSource={data_source}
       columns={column_type}
       loading={loading}
---
+++ constants/index.ts
@@ -0,0 +1,2 @@
+export const URI =
+  process.env.PUBLIC_NEXT_URI || "http://localhost:4000/graphql/";
