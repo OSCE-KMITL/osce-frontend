4b2a95154729aadb5be2ee1dac90732b38e0cb6b
aphisit.ths <aphisit.thu@gmail.com>
2023-04-23 19:14:39 +0700

    refactor : cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const path = require('path');
 require('dotenv').config();
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
+            },
+        ];
+    },
     reactStrictMode: false,
     images: {
         domains: ['lh3.googleusercontent.com'],
@@ -16,3 +33,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+// next.config.js
